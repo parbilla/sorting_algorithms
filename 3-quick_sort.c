de9932a46a7d5fe54112f0a7e0152fff44b2f4dc
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-02-01 19:07:39 -0300

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,25 +11,64 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int len = (int)size, pivot = array[len - 1], low = 0, i = -1, j = 0, tmp = 0;
+	if (array == NULL)
+		return;
+	the_really_quicksort(array, 0, (int)size - 1, (int)size);
+}
 
-	for (j = low; j <= len - 2; j++)
+/**
+ * the_really_quicksort - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm.
+ *
+ * @array: pointer to array.
+ * @low: low index.
+ * @high: high index.
+ *
+ * Return: Always 0
+ */
+void the_really_quicksort(int *array, int low, int high, int size)
+{
+	int p;
+
+	if (low < high)
 	{
-		printf("j: %i, low: %i\n", j, low);
+		p = partition(array, low, high, size);
+		the_really_quicksort(array, low, p - 1, size);
+		the_really_quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * partition - Lomuto method
+ *
+ * @array: pointer to array.
+ * @low: low index.
+ * @high: high index
+ *
+ * Return: i + 1
+ */
+
+int partition(int *array, int low, int high, int size)
+{
+	int pivot = array[high], i = (low - 1), j = 0, tmp = 0;
+
+	for (j = low; j <= high - 1; j++)
+	{
 		if (array[j] <= pivot)
 		{
-			printf("j: %i, low: %i\n", j, low);
 			i++;
-			printf("array[j]: %i, pivot: %i\n", array[j], pivot);
-			tmp = array[j];
-			array[j] = array[i];
-			array[i] = tmp;
-			print_array(array, size);
+			if (j != i)
+			{
+				tmp = array[j];
+				array[j] = array[i];
+				array[i] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
-	printf("array[i + 1]: %i, array[len]: %i\n", array[i + 1], array[len]);
 	tmp = array[i + 1];
-	array[i + 1] = array[len - 1];
-	array[len] = tmp;
+	array[i + 1] = array[high];
+	array[high] = tmp;
 	print_array(array, size);
+	return (i + 1);
 }
