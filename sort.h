3be32551cbb5740be1fafe5cce98409745b53050
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-02-01 19:09:37 -0300

    sort.h

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int low, int high, int size);
+void the_really_quicksort(int *array, int low, int high, int size);
 void swap_nodes(listint_t **head, listint_t *node, listint_t *next_node);
 #endif
