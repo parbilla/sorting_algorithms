645de2b70279d8f014f9dc87a7b28eb07e667434
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-02-02 10:47:01 -0300

    100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * sell_sort - function that sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence.
+ *
+ * @array: pointer to array of numbers.
+ * @size: number of elements in the array.
+ *
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+
+}
