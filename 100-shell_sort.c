4f7e6f346bc3ee7b0b21ad381fed39658db1c11b
parbilla <parbilla@gmail.com>
2021-02-02 15:34:27 +0000

    task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * sell_sort - function that sorts an array of integers in ascending order
+ * shell_sort - function that sorts an array of integers in ascending order
  * using the Shell sort algorithm, using the Knuth sequence.
  *
  * @array: pointer to array of numbers.
@@ -11,5 +11,35 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	int left, right, knuth, aux, flag;
 
+	if (array == NULL || size < 2)
+		return;
+
+	knuth = 1;
+	while (knuth <= (int)size / 3)
+	{
+		knuth = knuth * 3 + 1;
+	}
+
+	while (knuth > 0)
+	{
+		flag = 1;
+		for (left = 0; left < (int)size - knuth; left++)
+		{
+			right = left + knuth;
+			if (array[left] > array[right])
+			{
+				aux = array[left];
+				array[left] = array[right];
+				array[right] = aux;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			knuth = (knuth - 1) / 3;
+			print_array(array, size);
+		}
+	}
 }
