accec2a43bba5fdcf3ab2ae478864cfe3f394353
parbilla <parbilla@gmail.com>
2021-02-02 15:35:54 +0000

    task 4

--- sort.h
+++ sort.h
@@ -27,4 +27,6 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int low, int high, int size);
 void the_really_quicksort(int *array, int low, int high, int size);
 void swap_nodes(listint_t **head, listint_t *node, listint_t *next_node);
+void shell_sort(int *array, size_t size);
+
 #endif
